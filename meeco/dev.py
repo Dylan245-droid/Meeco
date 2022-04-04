3253ab90ca0f55da9cec667ae0e6ee34b4d109dd
Dylan ONDO <67437542+Dylan245-droid@users.noreply.github.com>
2022-04-04 16:28:11 +0100

    change xhitenoise middleware

--- meeco/dev.py
+++ meeco/dev.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.security.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
